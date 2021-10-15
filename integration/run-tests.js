066d0a09f57106b2470a32a2a56d910c46d2bc8e
Dmitry Ivanov <diq>
2021-10-15 04:37:39 +0400

    log

--- integration/run-tests.js
+++ integration/run-tests.js
@@ -2,26 +2,31 @@
   const cp = require('child_process')
   const fs = require('fs').promises
   const path = require('path')
+  const util = require('util')
 
   const dirname = path.join(module.path, 'screenshots')
   await fs.rm(dirname, { force: true, recursive: true })
   await fs.mkdir(dirname)
 
+  function log(...args) {
+    process.stdout.write(args.map(x => util.format(x)).join(" ") + "\n")
+  }
+
   const i = process.argv.indexOf("spawn")
   const prcs = i === -1 ? [] : process.argv.slice(i + 1).map(cmd => {
-    console.log("spawn", cmd)
+    log("spawn", cmd)
     const p = cp.spawn(cmd, { shell: true, stdio: 'ignore' })
     p.unref()
-    console.log("  id:", p.pid)
+    log("  id:", p.pid)
     return p
   })
 
   await new Promise(resolve => setTimeout(resolve, 5000))
 
   const selenium = cp.spawn("selenium-standalone start", { shell: true, stdio: 'pipe' })
-  console.log("Selenium process id:", selenium.pid)
+  log("Selenium process id:", selenium.pid)
 
-  selenium.stderr.pipe(process.stderr)
+  selenium.stderr.pipe(process.stderr, { end: false })
 
   await new Promise(resolve => {
     let log = ''
@@ -39,7 +44,7 @@
   })
 
   const hermione = cp.spawn("npm run hermione", { shell: true, stdio: 'inherit' })
-  console.log("Hermione process id:", hermione.pid)
+  log("Hermione process id:", hermione.pid)
   hermione.on('close', code => process.exit(code))
 
   process.on('exit', () => {
